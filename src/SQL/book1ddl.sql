a75f9e113d23f12e9aad1ccf4ad8fc72840395ec
Alvin Zhang <mosoft521@gmail.com>
2019-10-03 21:49:10 +0800

    20191003 db backup

--- src/SQL/book1ddl.sql
+++ src/SQL/book1ddl.sql
@@ -1,9 +1,10 @@
+
 /*==============================================================*/
 /* Table: t_book                                                */
 /*==============================================================*/
 create table t_book
 (
-   ID                   int(11) not null auto_increment comment '主键',
+   ID                   int(11) not null comment '主键',
    BOOK_NAME            varchar(50) comment '图书名称',
    BOOK_INTRO           varchar(200) comment '简介',
    BOOK_PRICE           double comment '图书单价',
@@ -12,6 +13,8 @@ create table t_book
    IMAGE_URL            varchar(200) comment '缩略图url',
    AUTHOR               varchar(200) comment '作者',
    REPERTORY_SIZE       bigint(10) comment '库存量',
+   CREATE_TIME          datetime not null comment '创建时间',
+   CREATE_BY            int(11) not null comment '创建人',
    primary key (ID)
 );
 
@@ -38,10 +41,12 @@ create index TYPE_ID_FK on t_book
 /*==============================================================*/
 create table t_book_in_record
 (
-   ID                   int(11) not null auto_increment comment '主键',
+   ID                   int(11) not null comment '主键',
    BOOK_ID              int(11) comment '图书',
    IN_RECORD_ID         int(11) comment '销售记录',
    IN_SUM               int(10) comment '入库数量',
+   CREATE_TIME          datetime not null comment '创建时间',
+   CREATE_BY            int(11) not null comment '创建人',
    primary key (ID)
 );
 
@@ -68,10 +73,12 @@ create index T_IN_RECORD_ID_FK on t_book_in_record
 /*==============================================================*/
 create table t_book_sale_record
 (
-   ID                   int(11) not null auto_increment comment '主键',
+   ID                   int(11) not null comment '主键',
    BOOK_ID              int(11) comment '图书',
    SALE_RECORD_ID       int(11) comment '图书销售记录',
    TRADE_SUM            int(10) comment '销售数量',
+   CREATE_TIME          datetime not null comment '创建时间',
+   CREATE_BY            int(11) not null comment '创建人',
    primary key (ID)
 );
 
@@ -98,9 +105,11 @@ create index T_SALE_RECORD_ID_FK on t_book_sale_record
 /*==============================================================*/
 create table t_book_type
 (
-   ID                   int(11) not null auto_increment comment '主键',
+   ID                   int(11) not null comment '主键',
    TYPE_NAME            varchar(50) comment '名称',
    TYPE_INTRO           varchar(200) comment '简介',
+   CREATE_TIME          datetime not null comment '创建时间',
+   CREATE_BY            int(11) not null comment '创建人',
    primary key (ID)
 );
 
@@ -111,8 +120,10 @@ alter table t_book_type comment '图书种类';
 /*==============================================================*/
 create table t_in_record
 (
-   ID                   int(11) not null auto_increment comment '主键',
+   ID                   int(11) not null comment '主键',
    RECORD_DATE          datetime comment '入库日期',
+   CREATE_TIME          datetime not null comment '创建时间',
+   CREATE_BY            int(11) not null comment '创建人',
    primary key (ID)
 );
 
@@ -123,11 +134,13 @@ alter table t_in_record comment '入库记录';
 /*==============================================================*/
 create table t_publisher
 (
-   ID                   int(11) not null auto_increment comment '主键',
+   ID                   int(11) not null comment '主键',
    PUB_NAME             varchar(50) comment '出版社名称',
    PUB_TEL              varchar(50) comment '出版社电话',
    PUB_LINK_MAN         varchar(50) comment '联系人',
    PUB_INTRO            varchar(200) comment '简介',
+   CREATE_TIME          datetime not null comment '创建时间',
+   CREATE_BY            int(11) not null comment '创建人',
    primary key (ID)
 );
 
@@ -138,8 +151,10 @@ alter table t_publisher comment '出版社';
 /*==============================================================*/
 create table t_sale_record
 (
-   ID                   int(11) not null auto_increment comment '主键',
+   ID                   int(11) not null comment '主键',
    RECORD_DATE          datetime comment '销售日期',
+   CREATE_TIME          datetime not null comment '创建时间',
+   CREATE_BY            int(11) not null comment '创建人',
    primary key (ID)
 );
 
@@ -153,6 +168,7 @@ create table t_user
    ID                   int(11) not null auto_increment comment '主键',
    USER_NAME            varchar(20) comment '用户名',
    USER_PASSWORD        varchar(20) comment '密码',
+   CREATE_TIME          datetime not null comment '创建时间',
    primary key (ID)
 );
 
@@ -164,14 +180,35 @@ alter table t_book add constraint FK_fk_book_2_publisher foreign key (PUB_ID)
 alter table t_book add constraint FK_fk_book_2_type foreign key (TYPE_ID)
       references t_book_type (ID) on delete restrict on update restrict;
 
+alter table t_book add constraint fk_book_2_user foreign key (CREATE_BY)
+      references t_user (ID) on delete restrict on update restrict;
+
 alter table t_book_in_record add constraint FK_fk_book_in_record_2_book foreign key (BOOK_ID)
       references t_book (ID) on delete restrict on update restrict;
 
 alter table t_book_in_record add constraint FK_fk_book_in_record_2_in_record foreign key (IN_RECORD_ID)
       references t_in_record (ID) on delete restrict on update restrict;
 
+alter table t_book_in_record add constraint FK_fk_book_in_record_2_user foreign key (CREATE_BY)
+      references t_user (ID) on delete restrict on update restrict;
+
 alter table t_book_sale_record add constraint FK_fk_book_sale_record_2_book foreign key (BOOK_ID)
       references t_book (ID) on delete restrict on update restrict;
 
 alter table t_book_sale_record add constraint FK_fk_book_sale_record_2_sale_record foreign key (SALE_RECORD_ID)
       references t_sale_record (ID) on delete restrict on update restrict;
+
+alter table t_book_sale_record add constraint FK_fk_book_sale_record_2_user foreign key (CREATE_BY)
+      references t_user (ID) on delete restrict on update restrict;
+
+alter table t_book_type add constraint FK_fk_type_2_user foreign key (CREATE_BY)
+      references t_user (ID) on delete restrict on update restrict;
+
+alter table t_in_record add constraint FK_fk_in_record_2_user foreign key (CREATE_BY)
+      references t_user (ID) on delete restrict on update restrict;
+
+alter table t_publisher add constraint FK_fk_publisher_2_user foreign key (CREATE_BY)
+      references t_user (ID) on delete restrict on update restrict;
+
+alter table t_sale_record add constraint FK_fk_sale_record_2_user foreign key (CREATE_BY)
+      references t_user (ID) on delete restrict on update restrict;
